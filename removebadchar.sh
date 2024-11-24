2f1ae6f0b7bf3ff69cf42b14f0ae63adf412f828
arcdeciel82 <curjlync@ut.utm.edu>
2024-11-24 09:17:49 -0600

    fix scan dir

--- removebadchar.sh
+++ removebadchar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find data/repo/ -type f -name '*.py' -print0 | 
+find data/python-trainingData/pythonFiles/ -type f -name '*.py' -print0 | 
     while IFS= read -r -d $'\0' filename; do 
         iconv -f utf-8 -t utf-8 -c "$filename" > "$filename".iconv_cleaned_utf8
         mv "$filename".iconv_cleaned_utf8 "$filename"
